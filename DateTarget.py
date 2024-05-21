dea2ebaef2f699222cd2b81e842297baa3840dc6
Md. Waliul Islam Rayhan <b190305034@cse.jnu.ac.bd>
2024-05-22 00:15:19 +0600

    Update Date Target Issue solved.

--- DateTarget.py
+++ DateTarget.py
@@ -20,31 +20,30 @@ def upload_data_to_firebase():
     for snapshot in power_plant_ref.get().items():
         power_plant_key = snapshot[0]
         power_plant_date_ref = power_plant_ref.child(power_plant_key).child('Date').child(current_date)
-        power_plant_date_ref.child('capacity').child('ppcurrentCapacity').set(0)
-        power_plant_date_ref.child('capacity').child('pptargetCapacity').set(0)
-        power_plant_date_ref.child('total').child('pptotalCurrentCapacity').set(0)
-        power_plant_date_ref.child('alert').set(False)
-        power_plant_date_ref.child('history').child('pptotalCurrentCapacity').set(0)
-        power_plant_date_ref.child('history').child('last_update_time').set('11.59.59 PM')
+        if not power_plant_date_ref.get():
+            power_plant_date_ref.child('capacity').child('ppcurrentCapacity').set(0)
+            power_plant_date_ref.child('capacity').child('pptargetCapacity').set(0)
+            power_plant_date_ref.child('total').child('pptotalCurrentCapacity').set(0)
+            power_plant_date_ref.child('alert').set(False)
 
     # Distributor data
     distributor_ref = ref.child('Distributor')
     for snapshot in distributor_ref.get().items():
         distributor_key = snapshot[0]
         distributor_date_ref = distributor_ref.child(distributor_key).child('Date').child(current_date)
-        distributor_date_ref.child('demand').child('ddcurrentDemand').set(0)
-        distributor_date_ref.child('demand').child('ddtargetdemand').set(0)
-        distributor_date_ref.child('total').child('ddtotalCurrentdemand').set(0)
-        distributor_date_ref.child('alert').set(False)
-        distributor_date_ref.child('history').child('ddtotalCurrentDemand').set(0)
-        distributor_date_ref.child('history').child('last_update_time').set('11.59.59 PM')
+        if not distributor_date_ref.get():
+            distributor_date_ref.child('demand').child('ddcurrentDemand').set(0)
+            distributor_date_ref.child('demand').child('ddtargetdemand').set(0)
+            distributor_date_ref.child('total').child('ddtotalCurrentdemand').set(0)
+            distributor_date_ref.child('alert').set(False)
 
     # Date data
     date_ref = ref.child('Date').child(current_date).child('total')
-    date_ref.child('AllppcurrentCapacity').set(0)
-    date_ref.child('AllpptargetCapacity').set(0)
-    date_ref.child('AllddcurrentDemand').set(0)
-    date_ref.child('AllddtargetDemand').set(0)
+    if not date_ref.get():
+        date_ref.child('AllppcurrentCapacity').set(0)
+        date_ref.child('AllpptargetCapacity').set(0)
+        date_ref.child('AllddcurrentDemand').set(0)
+        date_ref.child('AllddtargetDemand').set(0)
 
 # Call the function to upload data
 upload_data_to_firebase()
